9355f1ad12bae87fb9f63b5622f27e138b8fa363
Devanoyudhistira <devanotira@gmail.com>
2026-09-20 11:29:05 +0700

    fix:type on submit button

--- src/components/navbar-text-editor.tsx
+++ src/components/navbar-text-editor.tsx
@@ -2,7 +2,7 @@ import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
 import Submitbutton from "./submit-footer";
 
-export default function Navbartexteditor({postbutton}){
+export default function Navbartexteditor({postbutton}:{postbutton: () => void;}){
     return <nav className="bg-slate-50 shadow-2xs  w-screen h-13 px-6 py-1 flex items-center justify-between" > 
     <Link href={"/profile"} >
     <ArrowLeft className="size-5" />
--- src/components/submit-footer.tsx
+++ src/components/submit-footer.tsx
@@ -1,6 +1,10 @@
 import { Button } from "./ui/button";
+export type SubmitbuttonProps = {
+  postcontent: () => void;
+  
+};
 
-export default function Submitbutton({postcontent}) {
+export default function Submitbutton({postcontent}:SubmitbuttonProps) {
     return <div className=" py-1 flex justify-end gap-2 items-center" >
         <Button onClick={() => postcontent()} className={"text-xl font-semibold"} size={"sm"} variant={"destructive"} > Publish </Button>
         <Button className={"text-xl font-semibold"} size={"sm"} variant={"default"} > Preview </Button>
